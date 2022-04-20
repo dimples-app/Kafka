5608c3029ffc045cb9046fc3e62ec45f963f3c67
ddoshi2021 <ddoshi2021@gmail.com>
2022-04-19 22:20:35 -0700

    initial toipic

--- src/main/java/KafkaConfig/KafkaTopicConfig.java
+++ src/main/java/KafkaConfig/KafkaTopicConfig.java
@@ -1,7 +1,14 @@
 package KafkaConfig;
 
+import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
 public class KafkaTopicConfig {
+
+    public NewTopic createNewTopic() {
+        return TopicBuilder.name("First Topic")
+                .build();
+    }
 }
