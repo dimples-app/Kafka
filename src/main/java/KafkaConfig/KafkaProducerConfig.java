5492a04e6ceb01f54bef49c00a420cf03bb3a500
ddoshi2021 <ddoshi2021@gmail.com>
2022-04-20 21:51:34 -0700

    producer config

---
+++ src/main/java/KafkaConfig/KafkaProducerConfig.java
@@ -0,0 +1,13 @@
+package KafkaConfig;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KafkaProducerConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootStrapServer;
+
+
+}
